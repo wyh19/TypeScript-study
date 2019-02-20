0f7ae71ccdbad1f14853f522c896462875dd1357
王焱辉 <wyh_19@163.com>
2019-02-20 17:42:43 +0800

    update

--- src/1-基础类型.ts
+++ src/1-基础类型.ts
@@ -16,3 +16,14 @@ let age = 31;
 let sentence: string = `Hello,my name is ${ name1 },I'll be ${ age } years old.`
 
 // 数组
+//1:使用[]限定数组类型
+let list:number[] = [1,2,3];
+//2:使用数组泛型Array<number>
+let list2:Array<number> = [1,2,3];
+
+//元组,表示一个已知元素数量和类型的数组，各类型不必相同
+let x:[string,number] = ['hello',10];
+//通过索引获取的值也是限定好类型的
+
+
+
