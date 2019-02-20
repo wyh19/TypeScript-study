150ca28b1086d1e1c91855e6949bbd279b9a0737
王焱辉 <wyh_19@163.com>
2019-02-20 17:09:17 +0800

    update

--- src/1-基础类型.ts
+++ src/1-基础类型.ts
@@ -1,1 +1,15 @@
-let  isDone:boolean = false
+// 布尔
+let  isDone:boolean = false;
+
+//数值
+let decLiteral:number = 6;
+let hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+let octalLiteral: number = 0o744;
+let floatNum:number = 3.14;
+
+//字符串
+let name1:string = 'bob';
+//注意：上面没用name做为变量，是因为和ts源码的name冲突
+name1 = 'smith';
+
