d5058c0c0bea8009e315c4b80afb7b7845bb0102
王焱辉 <wyh_19@163.com>
2019-02-20 17:16:36 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## 关于
 这是[官方文档](https://www.tslang.cn/docs/handbook/basic-types.html)的练习手册。为了加深印象和理解，对着文档挨个实践一遍，并总结在该项目中，便于分享和后续学习。
 
+## 为什么要学ts
+三大框架的源码都是ts了，vscode也是ts的，如果想做一些大项目或者学习一些顶级开源项目的源码，必须得精通ts了。
+
 ## 编译ts
 首先安装typescript
 ```
--- src/1-基础类型.ts
+++ src/1-基础类型.ts
@@ -1,15 +1,18 @@
 // 布尔
-let  isDone:boolean = false;
+let isDone: boolean = false;
 
 //数值
-let decLiteral:number = 6;
+let decLiteral: number = 6;
 let hexLiteral: number = 0xf00d;
 let binaryLiteral: number = 0b1010;
 let octalLiteral: number = 0o744;
-let floatNum:number = 3.14;
+let floatNum: number = 3.14;
 
 //字符串
-let name1:string = 'bob';
+let name1: string = 'bob';
 //注意：上面没用name做为变量，是因为和ts源码的name冲突
 name1 = 'smith';
+let age = 31;
+let sentence: string = `Hello,my name is ${ name1 },I'll be ${ age } years old.`
 
+// 数组
