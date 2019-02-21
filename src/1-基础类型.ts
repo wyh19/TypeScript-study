050ae96fe4aae21939399afa6093d060445dbc1f
王焱辉 <wyh_19@163.com>
2019-02-21 11:05:38 +0800

    练习

--- src/1-基础类型.ts
+++ src/1-基础类型.ts
@@ -29,5 +29,12 @@ let x:[string,number] = ['hello',10];
 enum Color {Red, Green, Blue}
 let c: Color = Color.Green;
 
+//any
+let notSure: any = 4;
+notSure = "maybe a string instead";
+notSure = false; // okay, definitely a boolean
+let notSure1: any = 4;
+notSure1.ifItExists(); // okay, ifItExists might exist at runtime
+notSure1.toFixed(); // okay, toFixed exists (but the compiler doesn't check)
 
 
