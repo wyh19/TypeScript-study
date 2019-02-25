3f2e3b376a0d9005e54665014a4705782a873e46
王焱辉 <wyh_19@163.com>
2019-02-25 21:55:00 +0800

    练习

--- src/1-基础类型.ts
+++ src/1-基础类型.ts
@@ -51,7 +51,36 @@ let n: null = null;
 //默认情况下null和undefined是所有类型的子类型。 就是说你可以把 null和undefined赋值给number类型的变量。
 let un: number = u;
 
-//never
+//never，never类型表示的是那些永不存在的值的类型。
+// 例如， never类型是那些总是会抛出异常或根本就不会有返回值的函数表达式或箭头函数表达式的返回值类型； 
+// 返回never的函数必须存在无法达到的终点
+function error(message: string): never {
+    throw new Error(message);
+}
 
+// 推断的返回值类型为never
+function fail() {
+    return error("Something failed");
+}
 
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+    while (true) {
+    }
+}
 
+//object表示非原始类型，也就是除number，string，boolean，symbol，null或undefined之外的类型。
+declare function create(o: object | null): void;
+
+create({ prop: 0 }); // OK
+create(null); // OK
+// create(42); // Error
+// create("string"); // Error
+// create(false); // Error
+// create(undefined); // Error
+
+//类型断言
+let someValue: any = "this is a string";
+let strLength: number = (<string>someValue).length;
+let strLength1: number = (someValue as string).length;
+//当你在TypeScript里使用JSX时，只有 as语法断言是被允许的。
